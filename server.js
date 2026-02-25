c196f0f583685b2231c913089cb92f5487c727a3
ju3tin <ju3tin@hotmail.co.uk>
2026-02-25 20:51:53 +0000

    df

---
+++ server.js
@@ -0,0 +1,53 @@
+const WebSocket = require("ws");
+const http = require("http");
+
+const server = http.createServer();
+const wss = new WebSocket.Server({ server });
+
+const rooms = new Map();
+
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+
+  ws.on("message", (message) => {
+    let data;
+
+    try {
+      data = JSON.parse(message);
+    } catch {
+      return;
+    }
+
+    const { room, type, payload } = data;
+
+    if (!rooms.has(room)) {
+      rooms.set(room, new Set());
+    }
+
+    rooms.get(room).add(ws);
+
+    // Relay to other clients in room
+    rooms.get(room).forEach(client => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify({ type, payload }));
+      }
+    });
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+
+    rooms.forEach((clients, room) => {
+      clients.delete(ws);
+      if (clients.size === 0) {
+        rooms.delete(room);
+      }
+    });
+  });
+});
+
+const PORT = 8080;
+
+server.listen(PORT, () => {
+  console.log(`Signaling server running on ws://localhost:${PORT}`);
+});
